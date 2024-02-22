3217a8438170b9febb56ac20a76ce612e1641fd3
IonutMotoi <ionut.m.motoi@gmail.com>
2024-02-22 11:05:03 +0100

    Add simple EfficientNet BinaryClassifier model

---
+++ src/models.py
@@ -0,0 +1,21 @@
+import torch
+from torchvision import models
+
+
+class BinaryClassifier(torch.nn.Module):
+    def __init__(self):
+        super(BinaryClassifier, self).__init__()
+        self.efficientnet = models.efficientnet_b0(weights=models.EfficientNet_B0_Weights.IMAGENET1K_V1)
+        self.fc = torch.nn.Linear(1000, 2)
+
+    def forward(self, x):
+        x = self.efficientnet(x)
+        x = self.fc(x)
+        return x
+
+
+if __name__ == '__main__':
+    model = BinaryClassifier()
+
+    total_params = sum(p.numel() for p in model.parameters())
+    print("Total parameters:", total_params)
